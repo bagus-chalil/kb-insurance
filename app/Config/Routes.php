c1e46f73a41010328636e69d194834e6accab8dd
bagus-chalil <bagus.chalil@gmail.com>
2025-02-08 22:12:18 +0700

    upgrade to ci-4.6.0

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,29 +1,11 @@
 <?php
 
-namespace Config;
-
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
-
-// Load the system's routing file first, so that the app and ENVIRONMENT
-// can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
-	require SYSTEMPATH . 'Config/Routes.php';
-}
+use CodeIgniter\Router\RouteCollection;
 
 /**
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
+ * @var RouteCollection $routes
  */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(true);
-
+$routes->get('/', 'Home::index');
 /*
  * --------------------------------------------------------------------
  * Route Definitions
@@ -40,22 +22,3 @@ $routes->post('/assurance/save', 'CoverageController::simpan');
 $routes->get('/assurance/history', 'CoverageController::riwayat');
 $routes->get('/assurance/premi/(:num)', 'CoverageController::premi/$1');
 $routes->delete('/assurance/delete/(:num)', 'CoverageController::hapus/$1');
-
-
-/*
- * --------------------------------------------------------------------
- * Additional Routing
- * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
- */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
-	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
-}
